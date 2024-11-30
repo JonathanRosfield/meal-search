f48db8ac7c7d6d0a7e50a2ba52a65f933f5aba06
Jonathan Hernandez <jonahernandez2001@gmail.com>
2024-11-30 00:44:45 -0600

    Data Store and Meals by Letter complete

---
+++ src/store/state.js
@@ -0,0 +1,10 @@
+import MealsByIngredients from "@/views/MealsByIngredients.vue";
+import MealsByLetter from "@/views/MealsByLetter.vue";
+
+export default {
+    searchedMeals: [],
+    mealsByLetter: [],
+    mealsByIngredients: [],
+    ingredient: {}
+
+}
