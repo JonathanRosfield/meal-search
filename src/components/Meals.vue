e127b9d8b58da3089eb39ad88fd71c0d475cf08e
Jonathan Hernandez <jonahernandez2001@gmail.com>
2024-12-08 19:29:12 -0600

    Meals By Ingredient Done

--- src/components/Meals.vue
+++ src/components/Meals.vue
@@ -7,16 +7,13 @@ const { meals } = defineProps({
         type: Array
     }
 })
-
 </script>
 
-
 <template>
-<div class="grid grid-cols-3 gap-5 p-8">
-    <MealItem v-for="meal in meals" :key="meal.idMeal" :meal="meal"></MealItem>
-</div>
-<div v-if="!meals.length" class="flex justify-center text-gray-600">
-    Meals Not Found
-</div>
-
+    <div class="grid grid-cols-3 gap-5 p-8">
+        <MealItem v-for="meal in meals" :key="meal.idMeal" :meal="meal"></MealItem>
+    </div>
+    <div v-if="!meals.length" class="flex justify-center text-gray-600">
+        Meals Not Found
+    </div>
 </template>
